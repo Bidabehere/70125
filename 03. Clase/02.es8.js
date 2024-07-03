ced94d03a380cc89cdb8bc2e782b203199c0d9cc
JuanPablo <96244210+jbidabehere@users.noreply.github.com>
2024-07-02 21:22:11 -0300

    Clase 3

---
+++ 03. Clase/02.es8.js
@@ -0,0 +1,17 @@
+const usuario = {
+    name: "Andres",
+    email: "andreas@email.com",
+    age: 25
+};
+
+//Las entradas
+console.log(Object.entries(usuario));
+
+//Keys
+
+console.log(Object.keys(usuario));
+
+
+//values
+
+console.log(Object.values(usuario));
